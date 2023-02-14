93026e9ba343fd58b8b072f4369a8ea43a5e3f29
Lucas M. Braz <lmonteirobraz@gmail.com>
2023-02-14 15:15:54 -0300

    Update message

--- lib/hello.dart
+++ lib/hello.dart
@@ -20,7 +20,7 @@ class Hello extends StatelessWidget {
         ),
         const SizedBox(height: 28),
         DsText.body24(
-          "I'm a software engineer based in Toronto, Canada and also a communication and journalism student. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.",
+          "A Senior Flutter Developer based in Brazil. I'm passioned for UX and code quality, and I have hands-on experience with Test-Driven Development, BLoC, MobX, Redux, RESTful APIs and Material Design principles. I've published apps on both Play Store and App Store. I have over a decade of experience building high-quality mobile apps (half of it within Spotify), always bringing in a growth mindset and drive for excellence.",
         ),
         const Expanded(child: SizedBox(height: 28)),
         Row(
